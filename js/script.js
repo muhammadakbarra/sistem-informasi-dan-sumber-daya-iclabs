4c8fc632fc38af137ec2620fbdd98eb9c583509f
Rahmapuspitasari30 <rahma86788@gmail.com>
2024-09-18 17:59:53 +0800

    feat: Tampilan Home

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const check = document.querySelector("input");
+const ul = document.querySelector("ul");
+console.log(ul);
+
+check.addEventListener("click", function () {
+  ul.classList.toggle("slide");
+});
