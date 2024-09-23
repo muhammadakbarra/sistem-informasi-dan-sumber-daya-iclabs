d91c3d5ae1c3bc44ae89c0011b56fa80584985d5
WahyuSuat <yuusuat@gmail.com>
2024-09-24 01:49:55 +0800

    Feat: informasi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    // let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += 100;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= 100 ;
+    })
+})
+//entah apa
 const check = document.querySelector("input");
 const ul = document.querySelector("ul");
 console.log(ul);
