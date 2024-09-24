808b37efe5c14d8d769e36d027fcf1faa0edc1c6
tazzkirahamaliah <tazkirahamaliah@iraaa.local>
2024-09-24 21:31:03 +0800

    feat: sarana dan prasarana

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-const check = document.querySelector("input");
-const ul = document.querySelector("ul");
-console.log(ul);
-
-check.addEventListener("click", function () {
-  ul.classList.toggle("slide");
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll('.card');
+  
+    const observerOptions = {
+      threshold: 0.5 // Trigger when 50% of the card is visible
+    };
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        } else {
+          entry.target.classList.remove('in-view');
+        }
+      });
+    }, observerOptions);
+  
+    cards.forEach(card => {
+      observer.observe(card);
+    });
+  });
+  
